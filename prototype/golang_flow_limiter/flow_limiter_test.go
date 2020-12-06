cb842f9e1cab94bb5f80cd9d61e5a7f1be48465d
d2jvkpn <chenbin2018@protonmail.com>
2020-12-06 16:30:58 +0800

    ...

--- prototype/golang_flow_limiter/flow_limiter_test.go
+++ prototype/golang_flow_limiter/flow_limiter_test.go
@@ -33,3 +33,14 @@ func TestMain_t2(t *testing.T) {
 
 	fl.Close()
 }
+
+func TestMain_t3(t *testing.T) {
+	fl, _ := NewFlowLimiter(time.Second, 10)
+
+	for i := 0; i < 1000; i++ {
+		fmt.Println(time.Now().Format("2006-01-02T15:04:05.000Z07:00"), fl.Get())
+		time.Sleep(50 * time.Millisecond)
+	}
+
+	fl.Close()
+}
