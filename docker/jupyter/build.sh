cdd7babe99bbe4046c89178a3a9a5de0d76cb9e5
d2jvkpn <chenbin2018@protonmail.com>
2020-12-16 17:34:39 +0800

    update docker/jupyter/Dockerfile

---
+++ docker/jupyter/build.sh
@@ -0,0 +1,9 @@
+#! /usr/bin/env bash
+set -eu -o pipefail
+
+docker build --squash -t jupyter:latest .
+
+docker inspect jupyter:latest                |
+    jq -r ".[0].Comment"                     |
+    awk '{sub("sha256:", "", $2); print $2}' |
+    xargs -i docker rmi {}
