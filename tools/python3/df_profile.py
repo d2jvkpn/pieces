a38896acafe816ee4bbf25f00ebe7df8453a2e94
d2jvkpn <chenbin2018@protonmail.com>
2021-01-27 00:09:42 +0800

    df_profile.py with argsparse

--- tools/python3/df_profile.py
+++ tools/python3/df_profile.py
@@ -1,13 +1,24 @@
 #! /usr/bin/env python3
 
-import sys
+import os, argparse
 
 import pandas as pd
 from pandas_profiling import ProfileReport
 
-tsv, out = sys.argv[1:3]
+parser = argparse.ArgumentParser()
+parser.add_argument("-file", required=True, help="dataframe file")
+parser.add_argument("-out", default="", help="output file, default is html")
+parser.add_argument("-sep", default="\t", help="field seperator")
+args = parser.parse_args()
 
-df = pd.read_csv(tsv, sep="\t")
+file, out = args.file, args.out
+
+if out == "":
+    out = ".".join(file.split(".")[:-1]) + ".html"
+
+os.os.makedirs(os.path.dirname(os.path.abspath(file)), exist_ok=True)
+
+df = pd.read_csv(file, sep=args.sep)
 
 prof = ProfileReport(df)
 prof.to_file(output_file=out)
--- tools/python3/mysql_export_df.py
+++ tools/python3/mysql_export_df.py
@@ -1,3 +1,5 @@
+#! /usr/bin/env python3
+
 import os, sys, datetime
 
 import pandas as pd
