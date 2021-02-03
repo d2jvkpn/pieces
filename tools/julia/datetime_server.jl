21ec7eabbe50eeb7066513df1392365ab4a90290
d2jvkpn <chenbin2018@protonmail.com>
2021-02-04 07:10:24 +0800

    add datetime_servre.jl

---
+++ tools/julia/datetime_server.jl
@@ -0,0 +1,23 @@
+using Genie
+using Genie.Router
+
+using Dates
+using TimeZones
+
+
+function currentTime()
+  a = now()
+  ZonedDateTime(year(a), month(a), day(a), hour(a), minute(a), second(a), millisecond(a), localzone())
+end
+
+
+Genie.config.run_as_server = true
+port = length(ARGS) > 0 ? parse(Int64, ARGS[1]) : 8000
+
+
+route("/") do
+  currentTime()
+end
+
+println("starting service $(port)")
+Genie.startup(port=port)
