75667d395774cf501bb0e2049d34d3620698c4a5
d2jvkpn <chenbin2018@protonmail.com>
2021-02-08 22:59:36 +0800

    add comment to prototype/go/grpc/scripts/generate.sh

--- prototype/go/grpc/scripts/generate.sh
+++ prototype/go/grpc/scripts/generate.sh
@@ -1,6 +1,5 @@
 #! /bin/bash
 
-#! /usr/bin/env bash
 set -eu -o pipefail
 
 go mod init x
@@ -13,6 +12,9 @@ go install google.golang.org/grpc/cmd/protoc-gen-go-grpc
 # protoc greet/greetpb/greet.proto --go-grpc_out=.
 
 # protoc --go_out=plugins=grpc:. pkg/greetpb/greet.proto
+
 protoc --go_out=./  --go-grpc_out=./  pkg/greetpb/greet.proto
+# comment greet_grpc.pb.go::GreetServiceServer/mustEmbedUnimplementedGreetServiceServer()
 
 protoc --go_out=./  --go-grpc_out=./  pkg/calculatorpb/calculator.proto
+# comment calculator_grpc.pb.go::CalculatorServiceServer/mustEmbedUnimplementedCalculatorServiceServer()
