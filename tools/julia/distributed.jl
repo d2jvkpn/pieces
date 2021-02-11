693e756eb8fbb657f0a7864607039743762739fa
d2jvkpn <chenbin2018@protonmail.com>
2021-02-11 14:47:10 +0800

    add tools/julia/distributed.jl

---
+++ tools/julia/distributed.jl
@@ -0,0 +1,33 @@
+using Distributed
+
+using TOML
+
+#= example
+addprocs(
+    [("root@172.17.0.2:22", 2)];
+    dir="/root/wk_julia",
+    exename="/opt/julia-1.5.3/bin/julia",
+    tunnel=true,
+)
+=#
+
+function addmachine(c)
+    println(">>> connect to machine $(c["name"]) => $(c["addr"])")
+
+    addprocs(
+        [(c["addr"], c["n"])];
+        dir=c["dir"], exename=c["exename"], tunnel=true,
+    )
+end
+
+config = TOML.parsefile("config.toml")
+machines = config["machine"]
+
+println("### found $(length(machines)) machines")
+
+for i in 1:length(machines)
+    addmachine(machines[i])
+    machines[i]["id"] = i+1
+end
+
+println(">>> nworkers: $(nworkers())")
