9805565fa9a1554256c9b1976c4b4fcf7b82421b
d2jvkpn <chenbin2018@protonmail.com>
2021-02-25 18:30:33 +0800

    add items to prototype/go

---
+++ prototype/go/run_on_different_os/echo_test.go
@@ -0,0 +1,10 @@
+package x1
+
+import (
+	"testing"
+)
+
+// $ go tets -run=TestEcho
+func TestEcho(t *testing.T) {
+	Echo()
+}
---
+++ prototype/go/run_on_different_os/echo_unix.go
@@ -0,0 +1,10 @@
+package x1
+
+import (
+	"fmt"
+)
+
+func Echo() {
+	fmt.Println(">>> unix")
+	fmt.Println("    Hello, world!")
+}
