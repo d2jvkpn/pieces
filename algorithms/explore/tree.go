cb0885d1f54b81f90d436097c410411a2b7df539
d2jvkpn <chenbin2018@protonmail.com>
2021-03-14 08:18:26 +0800

    tree NewNode2 factory function

--- algorithms/explore/tree.go
+++ algorithms/explore/tree.go
@@ -5,14 +5,25 @@ import (
 )
 
 type Node struct {
-	Value  int
-	Parent *Node
-	Left   *Node
-	Right  *Node
+	Value       int
+	Parent      *Node
+	Left, Right *Node
 }
 
 func NewNode(value int) *Node {
 	return &Node{Value: value}
+}
+
+func NewNode2(value int, parent *Node, do func(node *Node)) (out *Node) {
+	out = &Node{Value: value, Parent: parent}
+	if do == nil {
+		return
+	}
+
+	for ; parent != nil; parent = parent.Parent {
+		do(parent)
+	}
+	return
 }
 
 func (node *Node) String() string {
