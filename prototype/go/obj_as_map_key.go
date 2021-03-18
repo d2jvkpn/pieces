6665b8fbaf99a7db28850b6bb46e93002a7a261f
d2jvkpn <chenbin2018@protonmail.com>
2021-03-18 20:21:50 +0800

    ...

--- prototype/go/obj_as_map_key.go
+++ prototype/go/obj_as_map_key.go
@@ -9,12 +9,32 @@ func main() {
 	mp := make(map[Data]bool, 1)
 
 	d := Data{Ip: "127.0.0.1", Port: 1991}
+	d.Pointer = new(int)
 	mp[d] = true
 
 	fmt.Printf("%#v\n", d)
+
+	inMap := func() (ok bool) {
+		_, ok = mp[d]
+		return ok
+	}
+
+	fmt.Println(inMap()) // true
+
+	*(d.Pointer) = 10
+	fmt.Println(inMap()) // true
+
+	d.Pointer = nil
+	fmt.Println(inMap()) // false
 }
 
 type Data struct {
 	Ip   string `json:"ip"`
 	Port int    `json:"port"`
+	// E    func() string // invalid map key type Data
+	Pointer *int `json:"pointer"`
+}
+
+func (d *Data) Echo() string {
+	return "hello"
 }
