76ccf6ad26be238ebddcd8779c014850a1fdfd1c
d2jvkpn <chenbin2018@protonmail.com>
2021-05-04 16:42:03 +0800

    add TgzDirs.sh

---
+++ tools/bash/TgzDirs.sh
@@ -0,0 +1,25 @@
+#! /usr/bin/env bash
+set -eu -o pipefail
+
+if [ $# -gt 0 ]; then
+   targets=$*
+else
+   targets=$(ls -d */)
+fi
+
+SAVEIFS=$IFS
+IFS=$(echo -en "\n\b")
+for d in $targets; do
+    echo ">>> tar $d"
+    tar -I pigz -cf "${d%/}.tar.gz" "$d" && rm -r "$d"
+done
+IFS=$SAVEIFS
+
+
+exit 0
+
+## alternative
+find * -prune -type d | while IFS= read -r d; do 
+    tar -cf "$d.tar" "$d" && rm -r "$d"
+done
+# find . -printf "%y %p\n"
