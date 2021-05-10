ccb408fc8aacfa0dca08741decd7d860d1a7d31a
d2jvkpn <chenbin2018@protonmail.com>
2021-05-11 07:57:00 +0800

    ...

--- docker/datetime/deployment/build.sh
+++ docker/datetime/deployment/build.sh
@@ -7,22 +7,18 @@ BRANCH="$1"
 path=$(dirname $0)
 test -z $registry && { echo "DOCKER_Registry is unset"; exit 1; }
 
-if [[ "$BRANCH" != "test" && "$BRANCH" != "main" ]]; then
-    echo "inlvaid app env for deployment!!!"
-    exit -1
-fi
 image="$registry/datetime:${BRANCH}"
 
 #### build local image
-echo ">>> building image: $image"
-git checkout -f $BRANCH
+echo ">>> Building image: $image"
 docker pull golang:1.16-alpine
 docker pull alpine
+git checkout -f $BRANCH
 docker build -f $path/Dockerfile --no-cache -t "$image" .
 docker image prune --force --filter label=stage=datetime_builder
 
 
 #### push to registry
-echo ">>> pushing image: $image"
+echo ">>> Pushing image: $image"
 docker push $image
 # sudo docker push $image
--- docker/datetime/deployment/deploy.sh
+++ docker/datetime/deployment/deploy.sh
@@ -6,21 +6,9 @@ wd=$(pwd)
 path=$(dirname $0)
 registry=$(printenv DOCKER_Registry)
 BRANCH="$1"
-PORT=""
+PORT="$2"
 test -z $registry && { echo "DOCKER_Registry is unset"; exit 1; }
 
-case $BRANCH in
-  "test")
-    PORT="1032"
-    ;;
-  "main")
-    PORT="1030"
-    ;;
-  *)
-    echo "inlvaid BRANCH for build!!!"
-    exit -1
-    ;;
-esac
 image="$registry/datetime:${BRANCH}"
 
 
@@ -32,5 +20,5 @@ export PORT=${PORT}
 envsubst < $path/deployment.yaml > docker-compose.yaml
 docker-compose up -d
 
-echo ">>> http server port: $PORT"
+echo ">>> HTTP server port: $PORT"
 docker logs datetime_${BRANCH}_service
