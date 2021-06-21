80f05824bc074a4e9ac50d31487e97d23df2e7df
d2jvkpn <chenbin2018@protonmail.com>
2021-06-21 22:57:55 +0800

    ...

--- pkg/go/misc/panic.go
+++ pkg/go/misc/panic.go
@@ -27,8 +27,8 @@ func GetPanic(n int) {
 
 func simplifyDebugStack(bts []byte, n int) string {
 	strs := strings.Split(strings.TrimSpace(string(bts)), "\n")
-	b := new(strings.Builder)
-	b.WriteString(strs[0] + "\n")
+	builder := new(strings.Builder)
+	builder.WriteString(strs[0] + "\n")
 
 	max := (len(strs) - 7) / 2
 	if n < 1 || n > max {
@@ -37,12 +37,12 @@ func simplifyDebugStack(bts []byte, n int) string {
 
 	for i := 7; i < 2*n+7; i++ {
 		if i%2 == 1 {
-			b.WriteString(strings.Split(strs[i], "(")[0])
+			builder.WriteString(strings.Split(strs[i], "(")[0])
 		} else {
 			t := filepath.Base(strings.Fields(strs[i])[0])
-			b.WriteString("(" + t + ")\n")
+			builder.WriteString("(" + t + ")\n")
 		}
 	}
 
-	return strings.TrimSpace(b.String())
+	return strings.TrimSpace(builder.String())
 }
