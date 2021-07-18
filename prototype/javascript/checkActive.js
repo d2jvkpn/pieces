ab07db7146388c46b065117c2b5f735afe5f1755
d2jvkpn <chenbin2018@protonmail.com>
2021-07-19 00:54:30 +0800

    ...

--- prototype/javascript/checkActive.js
+++ prototype/javascript/checkActive.js
@@ -1,6 +1,7 @@
+////
 let n = 0;
 let maxN = 5;
-let interval = 2000; // 2s
+let interval = 2000;
 
 let checkActive = setInterval(
   () => {
@@ -12,5 +13,42 @@ let checkActive = setInterval(
     }
     console.log(">>>", n, new Date());
   }
-, interval); // 2s
+, interval);
 
+////
+let activity = {
+  interval: 20*1000,
+  interval2: 30*1000,
+  delta: 10*1000,
+  last: new Date(),
+  n: 0,
+  m: 2,
+};
+
+updateActivity = () => {
+  activity.last = new Date();
+  activity.n+=1;
+}
+
+let cycle = setInterval(
+  () => {
+    let dura = new Date() - activity.last;
+    let d1 = activity.interval2 + activity.delta;
+    let d2 = activity.m*activity.interval2 + activity.delta;
+
+    if (activity.n === 0) {
+      if (dura > d1) console.log(`INFO: not initialized!`);
+      return;
+    }
+
+    if (dura > d1) {
+       console.log(`WARN: no activity for ${dura/1000}s, lastActivity=${activity.last}`);
+    }
+
+    if (dura > d2) {
+      console.log(`ERROR: no activity for ${dura/1000}s, lastActivity=${activity.last}`);
+      clearInterval(cycle);
+      return;
+    }
+  },
+activity.interval);
