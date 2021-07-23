b5f6f328ac6d50b806e46d61247b0fe0ab7a3e54
d2jvkpn <chenbin2018@protonmail.com>
2021-07-23 22:06:06 +0800

    Errorx.Check

--- pkg/go/errorx/error_instance.go
+++ pkg/go/errorx/error_instance.go
@@ -6,69 +6,80 @@ import (
 
 ///
 func ParseDataFailed(err error, msg string) (errx ErrorX) {
-	return ErrorX{
+	errx = ErrorX{
 		Kind:     "parse data failed",
 		Code:     -20,
 		HttpCode: http.StatusBadRequest,
 		Message:  msg,
 		Err:      err,
 	}
+
+	return errx.Check()
 }
 
 func InvalidParameter(err error, msg string) (errx ErrorX) {
-	return ErrorX{
+	errx = ErrorX{
 		Kind:     "invalid parameter",
 		Code:     -21,
 		HttpCode: http.StatusBadRequest,
 		Message:  msg,
 		Err:      err,
 	}
+
+	return errx.Check()
 }
 
 func NotFound(err error, msg string) (errx ErrorX) {
-	return ErrorX{
+	errx = ErrorX{
 		Kind:     "not found",
 		Code:     -22,
 		HttpCode: http.StatusBadRequest,
 		Message:  msg,
 		Err:      err,
 	}
+
+	return errx.Check()
 }
 
 func Conflict(err error, msg string) (errx ErrorX) {
-	return ErrorX{
+	errx = ErrorX{
 		Kind:     "status conflict",
 		Code:     -23,
 		HttpCode: http.StatusConflict,
 		Message:  msg,
 		Err:      err,
 	}
+
+	return errx.Check()
 }
 
 func NotFound2(err error, msg string) (errx ErrorX) {
-	return ErrorX{
+	errx = ErrorX{
 		Kind:     "not found",
 		Code:     20,
 		HttpCode: http.StatusNotFound,
 		Message:  msg,
 		Err:      err,
 	}
+
+	return errx.Check()
 }
 
 func InternalError(err error, msg string) (errx ErrorX) {
-	return ErrorX{
+	errx = ErrorX{
 		Kind:     "internal error",
 		Code:     21,
 		HttpCode: http.StatusInternalServerError,
 		Message:  msg,
 		Err:      err,
 	}
+
+	return errx.Check()
 }
 
-func (errx *ErrorX) Check() (out *ErrorX) {
+func (errx ErrorX) Check() (out ErrorX) {
 	if errx.Err == nil {
-		unexpected := ErrUnexpected()
-		return &unexpected
+		return ErrUnexpected()
 	}
 
 	if errx.Message == "" {
