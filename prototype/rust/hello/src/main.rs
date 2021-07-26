897af8e2c8a6676102860db3b7d9df8a234c6fca
d2jvkpn <chenbin2018@protonmail.com>
2021-07-26 20:46:14 +0800

    ...

--- prototype/rust/hello/src/main.rs
+++ prototype/rust/hello/src/main.rs
@@ -6,6 +6,9 @@ use play::music::open as open_mp3;
 mod archive;
 use crate::archive::arch::archive_file;
 
+//
+use rand::Rng;
+
 fn main() {
     play::music::open("a01.mp3");
     open_mp3("a02.mp3");
@@ -13,4 +16,8 @@ fn main() {
     play::play_mp3("Butterfly - Smile");
 
     archive_file("project.txt");
+
+    let mut rng = rand::thread_rng();
+    let num: i64 = rng.gen_range(1..=6);
+    println!("random number: {}", num);
 }
