a00e118e69080a2bc2ac6a094cb8f4a2f93fa4d1
d2jvkpn <chenbin2018@protonmail.com>
2021-07-26 18:11:42 +0800

    ...

--- prototype/rust/a01.rs
+++ prototype/rust/a01.rs
@@ -5,10 +5,12 @@ use std::mem;
 fn main() {
     let d1 = new_d1();
     println!(">>> main d1: {:p}", &d1);
-    println!("memory size of d1: {}", mem::size_of_val(&d1));
 
     let d2 = d1;
     println!(">>> main d2: {:p}", &d2);
+
+    let d3 = d2;
+    println!(">>> main d3: {:p}", &d3);
 }
 
 struct D {
@@ -17,6 +19,6 @@ struct D {
 
 fn new_d1() -> D {
     let d = D { v: 1 };
-    println!(">>> new_d1: {:p}", &d);
+    println!(">>> new_d1: {:p}, size={}", &d, mem::size_of_val(&d));
     return d;
 }
