a49e3aa69576a14b7029b44715960d813a832392
d2jvkpn <chenbin2018@protonmail.com>
2021-07-26 18:04:26 +0800

    ...

--- prototype/rust/a01.rs
+++ prototype/rust/a01.rs
@@ -1,7 +1,14 @@
-fn main() {
-    let d = new_d1();
+#![allow(dead_code)]
 
-    println!(">>> main: {:p}", &d);
+use std::mem;
+
+fn main() {
+    let d1 = new_d1();
+    println!(">>> main d1: {:p}", &d1);
+    println!("memory size of d1: {}", mem::size_of_val(&d1));
+
+    let d2 = d1;
+    println!(">>> main d2: {:p}", &d2);
 }
 
 struct D {
@@ -10,7 +17,6 @@ struct D {
 
 fn new_d1() -> D {
     let d = D { v: 1 };
-
     println!(">>> new_d1: {:p}", &d);
     return d;
 }
