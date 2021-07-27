f046167292d7f773f5123f05d82735dd88ecb082
d2jvkpn <chenbin2018@protonmail.com>
2021-07-27 09:51:20 +0800

    ...

---
+++ prototype/rust/a03.rs
@@ -0,0 +1,30 @@
+#[derive(Debug)]
+struct Person {
+    name: String,
+}
+
+#[derive(Debug)]
+struct Dog<'a> {
+    name: String,
+    owner: &'a Person,
+}
+
+fn main() {
+    let p1 = Person {
+        name: String::from("John"),
+    };
+
+    let mut d1 = Dog {
+        name: String::from("Max"),
+        owner: &p1,
+    };
+
+    println!("{:?}, {:?}", d1, p1);
+
+    let p2 = Person {
+        name: String::from("unknown"),
+    };
+    d1.owner = &p2;
+
+    println!("{:?}, {:?}", d1, p1);
+}
