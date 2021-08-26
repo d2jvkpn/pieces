ab91050d0918048a19fcf9119bf7160ebb42959d
d2jvkpn <chenbin2018@protonmail.com>
2021-08-26 20:48:58 +0800

    ...

--- tools/bash/NPM_app.sh
+++ tools/bash/NPM_app.sh
@@ -4,6 +4,21 @@ set -eu -o pipefail
 wd=$(pwd)
 
 app=$1
-mkdir -p ~/Apps/$app && cd ~/Apps/$app
+target=~/Apps/$app
+
+test -d $target && rm -r $target
+mkdir -p $target && cd $target
 npm install -g $app
+
 cd $wd
+
+
+exit
+##### set registry
+npm config set registry http://registry.npm.taobao.org/
+
+##### list project installed packages
+npm list
+
+##### list global installed packaged
+npm list --global
