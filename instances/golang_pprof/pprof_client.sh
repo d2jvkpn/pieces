0dc44118ef1a40221c36c98467835e81b280d9fa
d2jvkpn <chenbin2018@protonmail.com>
2021-10-28 23:36:13 +0800

    ...

--- instances/golang_pprof/pprof_client.sh
+++ instances/golang_pprof/pprof_client.sh
@@ -20,10 +20,12 @@ wget -O heap.out         $addr/debug/pprof/heap?seconds=$secs &
 wget -O mutex.out        $addr/debug/pprof/mutex?seconds=$secs &
 wget -O profile.out      $addr/debug/pprof/profile?seconds=$secs &
 wget -O threadcreate.out $addr/debug/pprof/threadcreate?seconds=$secs &
-wget -O status.json      $addr/debug/runtime/status &
 
 # go tool trace trace.out
 wget -O trace.out        $addr/debug/pprof/trace?seconds=$secs &
+
+# go runtime
+wget -O status.json      $addr/debug/runtime/status &
 
 wait
 echo "done"
