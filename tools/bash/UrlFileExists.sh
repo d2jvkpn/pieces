1865d4d47c86313ad05ca72c2f6a4daabc4ac2f1
d2jvkpn <chenbin2018@protonmail.com>
2021-12-21 09:19:53 +0800

    ...

---
+++ tools/bash/UrlFileExists.sh
@@ -0,0 +1,15 @@
+#! /usr/bin/env bash
+set -eu -o pipefail
+
+_wd=$(pwd)
+_path=$(dirname $0 | xargs -i readlink -f {})
+
+
+url=$1
+output=$(2>&1 curl -s -I $url  | awk '{print; exit}')
+
+if [[ "$output" =~ "200 OK" ]]; then
+    echo true
+else
+    echo false
+fi
