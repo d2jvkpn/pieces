759778c3e409a832496787dee02da9b315394c40
d2jvkpn <chenbin2018@protonmail.com>
2021-12-28 10:51:20 +0800

    update test_03.rs

--- instances/search_in_file/tests/test_03.rs
+++ instances/search_in_file/tests/test_03.rs
@@ -1,7 +1,20 @@
 fn main() {
-    let mut buffer = [0; 16];
+    let mut buffer = [0; 10];
     buffer[0] = 65;
 
-    println!("{}", buffer.iter().filter(|&v| *v != 0).count());
-    println!("{:?}", String::from_utf8_lossy(&buffer).trim_matches(char::from(0)));
+    println!(
+        "notzeros={}, buffer={:?}",
+        buffer.iter().filter(|&v| *v != 0).count(),
+        String::from_utf8_lossy(&buffer).trim_matches(char::from(0)),
+    );
+
+    let slice = &mut buffer[5..];
+    println!("slice.len()={}", slice.len());
+    slice[0] = 65;
+
+    println!(
+        "notzeros={}, buffer={:?}",
+        buffer.iter().filter(|&v| *v != 0).count(),
+        String::from_utf8_lossy(&buffer).trim_matches(char::from(0)),
+    );
 }
