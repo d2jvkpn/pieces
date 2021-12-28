16e3fd3f469c8080253492c594ff2c6833843b68
d2jvkpn <chenbin2018@protonmail.com>
2021-12-28 14:11:02 +0800

    ...

--- instances/search_in_file/tests/test_04.rs
+++ instances/search_in_file/tests/test_04.rs
@@ -5,16 +5,20 @@ fn main() {
     let h2 = "你好";
     println!("{:?}", h2.as_bytes());
 
-    let mut v1 = vec![0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+    let mut v1 = vec![0, 1, 2, 3, 4, 5];
     // dbg!(&v1);
     println!(">>> v1 = {:?}", v1);
-    println!("    v1.len() = {}, v1.capacity()={}", v1.len(), v1.capacity());
+    println!("    v1.len() = {}, v1.capacity()={}", v1.len(), v1.capacity()); // 6, 6
 
     v1.drain(0..2); // drop the first two elements
     println!(">>> v1 = {:?}", v1);
-    println!("    v1.len() = {}, v1.capacity()={}", v1.len(), v1.capacity());
+    println!("    v1.len() = {}, v1.capacity()={}", v1.len(), v1.capacity()); // 4, 6
 
     v1.truncate(2); // keep the first two elements
     println!(">>> v1 = {:?}", v1);
-    println!("    v1.len() = {}, v1.capacity()={}", v1.len(), v1.capacity());
+    println!("    v1.len() = {}, v1.capacity()={}", v1.len(), v1.capacity()); // 2, 6
+
+    v1.extend_from_slice(&vec![6, 7, 8, 9, 10, 11]);
+    println!(">>> v1 = {:?}", v1);
+    println!("    v1.len() = {}, v1.capacity()={}", v1.len(), v1.capacity()); // 8, 12
 }
