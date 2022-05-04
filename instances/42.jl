7df844562bb6c924ce0447cf0d5505cf6812c9ac
d2jvkpn <chenbin2018@protonmail.com>
2022-05-04 09:15:00 +0800

    ...

--- instances/42.jl
+++ instances/42.jl
@@ -1,13 +1,12 @@
 using Printf
 
-println("1/42 = ", 1/42)
-println("1/24 = ", 1/24)
+println("Hello, Julia!")
+
+@printf("1/42 = %f, 1/24 = %f\n", 1/42, 1/24)
 
 @printf(
-  "%d, %d, %d, %d\n",
+  "Life, the Universe and Everything: %d, %d, %d, %d\n",
   Int(0b101010), Int(0x2a), Int('*'),
   (-80538738812075974)^3 + 80435758145817515^3 + 12602123297335631^3,
 )
 # https://news.mit.edu/2019/answer-life-universe-and-everything-sum-three-cubes-mathematics-0910
-
-println("Life, the Universe and Everything: ", 42)
