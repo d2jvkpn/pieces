365cb8c90e4bd517e6032cec4c650e963a52948b
d2jvkpn <chenbin2018@protonmail.com>
2022-05-12 17:20:32 +0800

    ...

--- instances/42/42.sh
+++ instances/42/42.sh
@@ -5,5 +5,6 @@ _path=$(dirname $0 | xargs -i readlink -f {})
 
 echo "Hello, world!"
 
-ans=$(echo "(-80538738812075974)^3 + 80435758145817515^3 + 12602123297335631^3" | bc)
+ans=$(bc <<< "(-80538738812075974)^3 + 80435758145817515^3 + 12602123297335631^3")
+# bc <<< "scale=3; 5/3"
 echo "Life, the Universe and Everything: $ans"
