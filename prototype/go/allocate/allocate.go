2d5863f2dbd815e998a439b2ab0f2f672338a91f
d2jvkpn <chenbin2018@protonmail.com>
2022-06-27 14:04:33 +0800

    prototype/go/allocate

---
+++ prototype/go/allocate/allocate.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+	"runtime"
+	"sync"
+	"time"
+	"unsafe"
+)
+
+type Data struct {
+	Id    int64
+	Name  string
+	Tags  []uint8
+	At    time.Time
+	mutex *sync.RWMutex
+}
+
+func main() {
+	var m1, m2 runtime.MemStats
+
+	runtime.ReadMemStats(&m1)
+
+	d := Data{
+		Id:    1,
+		Name:  "你好,",
+		Tags:  []uint8{'A', 'B', 'C'},
+		At:    time.Now(),
+		mutex: new(sync.RWMutex),
+	}
+
+	fmt.Printf(
+		"Sizes: d -> %d, d.Id -> %d, d.Name -> %d, d.Tags -> %d, d.At -> %d, d.mutex -> %d, *d.mutex -> %d\n",
+		unsafe.Sizeof(d),
+		unsafe.Sizeof(d.Id),
+		unsafe.Sizeof(d.Name),
+		unsafe.Sizeof(d.Tags),
+		unsafe.Sizeof(d.At),
+		unsafe.Sizeof(d.mutex),
+		unsafe.Sizeof(*d.mutex),
+	)
+
+	runtime.ReadMemStats(&m2)
+	fmt.Println(m1.Alloc, m2.Alloc, m2.Alloc-m1.Alloc)
+}
--- tools/go/http-inspect/go_build.sh
+++ tools/go/http-inspect/go_build.sh
@@ -6,6 +6,6 @@ _path=$(dirname $0 | xargs -i readlink -f {})
 prog=$(basename ${_path})
 
 mkdir -p target
-go build -o target/$prog main.go
-GOOS=windows GOARCH=amd64 go build -o target/$prog.exe main.go
+go build -ldflags="-w -s" -o target/$prog main.go
+GOOS=windows GOARCH=amd64 go build -ldflags="-w -s" -o target/$prog.exe main.go
 ls -al target
