bc9d481125519abf76898339b44a667c65e41c6a
d2jvkpn <chenbin2018@protonmail.com>
2023-01-17 12:38:48 +0800

    ...

--- tools/web_scraping/scraping_zhihu_ans_01.js
+++ tools/web_scraping/scraping_zhihu_ans_01.js
@@ -1,7 +1,10 @@
 var target = document.querySelectorAll(".AnswerCard")[0];
+// .MoreAnswers
+
+var data = `${document.URL}\n\n${target.innerText}`;
 
 var link = document.createElement("a");
-link.href = `data:text/plain;charset=utf8,${document.URL}\n${target.innerText.replace(/\n/g, "%0D%0A")}\n`;
+link.href = `data:text/plain;charset=utf8,${data.replace(/\n/g, "%0D%0A")}\n`;
 
 link.download = `${document.title}.md`;
 link.click();
