991c7247ac5735ff54ed6cb0049dd30eef9359e1
d2jvkpn <chenbin2018@protonmail.com>
2023-01-18 12:04:54 +0800

    ...

--- tools/web_scraping/scraping_img_01.js
+++ tools/web_scraping/scraping_img_01.js
@@ -1,20 +1,29 @@
-// let elems = document.getElementsByClassName("div-with-img");
+// var elems = document.getElementsByClassName("div-with-img");
 
-// let data = Array.prototype.map.call(elems, e => {
+// var data = Array.prototype.map.call(elems, e => {
 //    return e.firstChild.getAttribute("src");
 // });
 
 // element_type=img, class_name=target
 var elems = document.querySelectorAll(`img[target]`);
+var links = Array.prototype.map.call(elems, e => e.getAttribute("src"));
 
-let data = Array.prototype.map.call(elems, e => e.getAttribute("src"));
+var url = new URL(document.URL);
 
-const link = document.createElement("a");
+var at = new Date(); // .getTime()
+var day = `${at.getFullYear()}-${at.getMonth()+1}-${at.getDate()}`;
+var clock = `${at.getHours()}-${at.getMinutes()}-${at.getMinutes()}`;
+
+var data = {
+  url: document.URL,
+  title: document.title,
+  time: at.toISOString(),
+  links: links,
+}
+
+var link = document.createElement("a");
 link.href = `data:text/json,${encodeURIComponent(JSON.stringify(data))}`;
-
-let ts = new Date().getTime()
-link.download = `links.${ts}.json`;
+link.download = `scrap_${url.hostname}_${day}T${clock}.json`;
 link.click();
 
-// jq -r .[] links.xxxx.json | xargs -i wget -c {}
-// jq -r .[] links.xxxx.json | xargs -n 1 -P 8 -i wget -c {}
+// jq -r .links[] scrap_*.json | xargs -n 1 -P 8 -i wget -c {}
