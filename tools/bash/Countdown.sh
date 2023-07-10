0b6cff09574e495d9759e9c61fb1666da278ecf6
d2jvkpn <chenbin2018@protonmail.com>
2023-07-10 09:39:44 +0800

    ...

--- tools/bash/Countdown.sh
+++ tools/bash/Countdown.sh
@@ -5,14 +5,20 @@ _path=$(dirname $0 | xargs -i readlink -f {})
 
 if [[ $# -gt 0 && "$1" == *"-h"* ]]; then
     echo "$(basename $0) [timeout] [command] [arg...]"
-    echo -e "e.g.\n    timeout: 5, 1m, deault: 15\n    Countdown.sh 15 mpv ~/Downloads/sounds/01.wav"
+    echo -e "e.g.\n    timeout: 5s, 1m, deault: 15s"
+    echo -e "\n    Countdown.sh 15 mpv ~/Downloads/sounds/01.wav"
     exit 0
 fi
 
-secs=15
+secs=15s
 [ $# -gt 0 ] && secs="$1"
 shift
 cmd="$*"
+
+if [[ ! "$secs" =~ ^[0-9]+(m|s)$ ]]; then
+    echo "invalid time interval" >&2
+    exit 1
+fi
 
 if [[ "$secs" == *"s" ]]; then
     secs=${secs%s}
@@ -20,8 +26,10 @@ elif [[ "$secs" == *"m" ]]; then
     secs=$((${secs%m} * 60))
 fi
 
+sp="-\|/"; j=1
+
 for i in $(seq 1 $secs | tac); do
-    echo -en "\r==> $(date +%FT%T%:z) $(printf "%03d" $i)"
+    echo -en "\r==> $(date +%FT%T%:z) $(printf "%03d" $i) ${sp:j++%${#sp}:1}"
     sleep 1
 done
 
